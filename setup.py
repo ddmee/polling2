74d430eaacdb1ba5dddabd5cfd99dbf8b09520e2
Donal Mee <donal.mee@bromium.com>
2019-06-04 18:22:11 +0100

    Remove mock and pytest from package install requirements
    These are only needed for testing the package.
    Now using pytest through setup.py test alias to run tests.

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
-
+import os
 from setuptools import setup, find_packages
 from polling2 import __version__
 
-with open("./requirements.txt") as fp:
-    requirements = fp.read()
-    requirements = requirements.split("\n")
+# consts
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md"), "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+with open(os.path.join(HERE, "test_requirements.txt")) as fp:
+    TEST_REQUIREMENTS = fp.read().split("\n")
 
 setup(
     name='polling2',
-    description='Powerful polling utility with many configurable options',
     version=__version__,
     author='Donal Mee',
+    description='Powerful polling utility with many configurable options',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     url='http://github.com/ddmee/polling2',
     download_url='',
     py_modules=['polling2'],
-    install_requires=requirements,
-    test_suite='tests'
+    tests_require=TEST_REQUIREMENTS,
+    test_suite='tests',
+    setup_requires=['pytest-runner',],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
